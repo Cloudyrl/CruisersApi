a294c78f64dd2c2841463a539ad1096f3eb0b356
Rommel Liendo <cloudyrl1@gmail.com>
2019-10-03 14:13:41 -0400

    BaseResponse class implemented to handle abse errors for the response

--- Domain/Communication/BaseResponse.cs
+++ Domain/Communication/BaseResponse.cs
@@ -1,7 +1,14 @@
 ﻿namespace CruisersApi.Domain.Communication
 {
-    public class BaseResponse
+    public abstract class BaseResponse
     {
-        
+        public bool Success { get; protected set; }
+        public string Message { get; protected set; }
+
+        public BaseResponse(bool success, string message)
+        {
+            Success = success;
+            Message = message;
+        }
     }
 }
--- Domain/Communication/SaveCruiserResponse.cs
+++ Domain/Communication/SaveCruiserResponse.cs
@@ -1,7 +1,17 @@
-﻿namespace CruisersApi.Domain.Communication
+﻿using CruisersApi.Domain.Entities;
+
+namespace CruisersApi.Domain.Communication
 {
-    public class SaveCruiserResponse
+    public class SaveCruiserResponse: BaseResponse
     {
-        
+        public Cruiser Cruiser;
+        private SaveCruiserResponse(bool success, string message, Cruiser cruiser) : base(success, message)
+        {
+            Cruiser = cruiser;
+        }
+
+        public SaveCruiserResponse(Cruiser cruiser) : this(true, string.Empty, cruiser){}
+
+        public SaveCruiserResponse(string message) : this(false, message, null){}
     }
 }
