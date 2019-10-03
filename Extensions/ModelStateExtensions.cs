e7c351bbd9e707e75ca0aa5d1f96db0b474f2347
Rommel Liendo <cloudyrl1@gmail.com>
2019-10-03 14:16:41 -0400

    added ModelStateExtensions class to extract errors from the model State, also added constraint to SaveCruiserDto

--- Extensions/ModelStateExtensions.cs
+++ Extensions/ModelStateExtensions.cs
@@ -1,7 +1,16 @@
-﻿namespace CruisersApi.Extensions
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace CruisersApi.Extensions
 {
-    public class ModelStateExtensions
+    public static class ModelStateExtensions
     {
-        
+        public static List<string> GetErrorMessages(this ModelStateDictionary dictionary)
+        {
+            return dictionary.SelectMany(m => m.Value.Errors)
+                .Select(m => m.ErrorMessage)
+                .ToList();
+        }
     }
 }
--- Resources/SaveCruiserDto.cs
+++ Resources/SaveCruiserDto.cs
@@ -1,7 +1,30 @@
-﻿namespace CruisersApi.Resources
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CruisersApi.Resources
 {
     public class SaveCruiserDto
     {
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
         
+        [Required]
+        public bool Status { get; set; }
+        
+        [Required]
+        public int Capacity { get; set; }
+        
+        [Required]
+        public int LoadingShipCap { get; set; }
+        
+        [Required]
+        [MaxLength(50)]
+        public string Model { get; set; }
+        
+        [Required]
+        [MaxLength(50)]
+        public string Line { get; set; }
+        
+        public string Picture { get; set; }
     }
 }
