7da5ceedb0a041909af6f43bad13d57698904783
Rommel Liendo <cloudyrl1@gmail.com>
2019-10-02 19:17:14 -0700

    Added Cruisers and Layover entities

---
+++ Domain/Entities/Layover.cs
@@ -0,0 +1,13 @@
+﻿namespace CruisersApi.Domain.Entities
+{
+    public class Layover
+    {
+        public int Id { get; set; }
+        public int CruiserId { get; set; }
+        public string DepartureDate { get; set; }
+        public string ArrivalDate { get; set; }
+        public decimal Price { get; set; }
+        public int LocDeparture { get; set; }
+        public int LocArrival { get; set; }
+    }
+}
