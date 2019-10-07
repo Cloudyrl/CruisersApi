68438ef82bc549ed2507af2fd707e7222dd47d77
Rommel Liendo <cloudyrl1@gmail.com>
2019-10-07 15:35:18 -0400

    added LayoverDto adnnd SabeLayoverDto to handle layover data comming from the Endpoint

---
+++ Resources/LayoverDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CruisersApi.Resources
+{
+    public class LayoverDto
+    {
+        public int Id { get; set; }
+        public DateTime DepartureDate { get; set; }
+        public DateTime ArrivalDate { get; set; }
+        public decimal Price { get; set; }
+        public int LocDeparture { get; set; }
+        public int LocArrival { get; set; }
+        public int CruiserId { get; set; }
+    }
+}
---
+++ Resources/SaveLayoverDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CruisersApi.Resources
+{
+    public class SaveLayoverDto
+    {
+        [Required]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}")]
+        public DateTime DepartureDate { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}")]
+        public DateTime ArrivalDate { get; set; }
+        [Required]
+        public decimal Price { get; set; }
+        [Required]
+        public int LocDeparture { get; set; }
+        [Required]
+        public int LocArrival { get; set; }
+        [Required]
+        public int CruiserId { get; set; }
+    }
+}
