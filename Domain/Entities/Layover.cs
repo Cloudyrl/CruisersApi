0107105bdde09d244c915fc11392bd7baf55f63c
Rommel Liendo <cloudyrl1@gmail.com>
2019-10-07 15:40:09 -0400

    added mock data for layover entity

--- Domain/Entities/Layover.cs
+++ Domain/Entities/Layover.cs
@@ -1,14 +1,15 @@
-﻿namespace CruisersApi.Domain.Entities
+﻿using System;
+
+namespace CruisersApi.Domain.Entities
 {
     public class Layover
     {
         public int Id { get; set; }
-        public string DepartureDate { get; set; }
-        public string ArrivalDate { get; set; }
+        public DateTime DepartureDate { get; set; }
+        public DateTime ArrivalDate { get; set; }
         public decimal Price { get; set; }
         public int LocDeparture { get; set; }
         public int LocArrival { get; set; }
-        
         public int CruiserId { get; set; }
         public Cruiser Cruiser { get; set; }
     }
